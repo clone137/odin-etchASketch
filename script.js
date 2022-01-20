e1c6a8bab65d60c9f00bd8393319daec94f95747
brad hendrickse <bradh@box.org.za>
2022-01-20 14:15:31 +0200

    added a limit to the size of the board and refactored a little

--- script.js
+++ script.js
@@ -1,35 +1,44 @@
 'use strict';
 
-const clearButton = document.querySelector('.clearButton');
+let boardSize = 512;
 
-clearButton.addEventListener('click', function () {
+const clearButton = document.querySelector('.clearButton');
+clearButton.addEventListener('click', handleClearButton);
+
+function handleMouseover(e) {
+  e.target.classList.add('orange');
+}
+
+function handleClearButton() {
   const allDivs = document.querySelectorAll('.gridItem');
   for (const div of allDivs) {
+    // NOTE not sure if it's necessary to remove the event listener if i'm removing the element, but it feels right doing so
+    div.removeEventListener('mouseover', handleMouseover);
     div.remove();
   }
-  let sideLength = prompt('how many columns/rows?');
+  let sideLength;
+  do {
+    sideLength = Number(prompt('how many columns/rows? (1-100)'));
+  } while (sideLength < 1 || sideLength > 100);
   drawGrid(sideLength);
-});
+}
 
 function drawGrid(sideLength) {
   const container = document.getElementById('container');
   container.style.setProperty('--gridSize', sideLength);
 
-  let cellPixelSize = 512 / sideLength;
+  let cellPixelSize = boardSize / sideLength;
   container.style.setProperty('--cellSize', `${cellPixelSize}px`);
 
   for (let i = 0; i < sideLength * sideLength; i++) {
     let cell = document.createElement('div');
-    // cell.innerText = i + 1;
     cell.classList.add('gridItem');
     container.appendChild(cell);
   }
 
   const allDivs = document.querySelectorAll('.gridItem');
   for (const div of allDivs) {
-    div.addEventListener('mouseover', function (e) {
-      e.target.classList.add('orange');
-    });
+    div.addEventListener('mouseover', handleMouseover);
   }
 }
 
